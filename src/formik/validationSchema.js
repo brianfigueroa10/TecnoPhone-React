77e4f1965d9bc059d87bd5b6b96f6022861e8d1f
Brian Figueroa <bryan09figueroa@gmail.com>
2023-09-12 14:24:52 -0300

    validacion checkout

--- src/formik/validationSchema.js
+++ src/formik/validationSchema.js
@@ -4,7 +4,7 @@ export const regEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-
 
 export const checkoutValidationSchema = Yup.object({
   name: Yup.string().required('Nombre es requerido'),
-  cellphone: Yup.string().required('Celular es requerido').matches(/^[0-9]+$/, 'Debe contener solo números'),
+  cellphone: Yup.string().required('Celular es requerido').matches(/^[0-9]{10}$/, 'El número de celular debe tener 10 dígitos válidos'),
   email: Yup.string().email('Correo electrónico no válido').required('El correo electrónico es requerido'),
   address: Yup.string().required('Dirección es requerido'),
   location: Yup.string().required('Localidad  requerido'),
